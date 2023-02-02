cd553001836f1c6ee32e6bc178f0b85330343565
Puszmen <piotr.zatorski91@gmail.com>
2023-02-02 19:18:38 +0100

    I made class Portal and PortalGraMax

--- NOWE/Rozdział 6/PortalGraMax/src/PortalGraMax.java
+++ NOWE/Rozdział 6/PortalGraMax/src/PortalGraMax.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-publci class PomocnikGry {
+class PomocnikGry {
     private static final String alfabet = "abcdefg";
     private int dlugoscPlanszy = 7;
     private int wielkoscPlanszy = 49;
@@ -69,6 +69,103 @@ publci class PomocnikGry {
         }
         return zajetePola;
     }
+}
+class Portal {
+    private ArrayList<String> polaPolozenia;
+    private String nazwa;
+
+    public void setPolaPolozenia(ArrayList<String> ppol) {
+        polaPolozenia = ppol;
+    }
+    public void setNazwa(String nzwPortalu) {
+        nazwa = nzwPortalu;
+    }
+
+    public String sprawdz (String ruch) {
+        String wynik = "pudło";
+        int indeks = polaPolozenia.indexOf(ruch);
+        if (indeks >= 0) {
+            polaPolozenia.remove(indeks);
+            if (polaPolozenia.isEmpty()) {
+                wynik = "zatopiony";
+                System.out.println("Auć! Zatopiłeś portal " + nazwa + " :( ");
+            } else {
+                wynik = "trafiony";
+            }
+        }
+        return wynik;
+
+    }
+}
+
+public class PortalGraMax {
+    private PomocnikGry pomocnik = new PomocnikGry();
+    private ArrayList<Portal> listaPortali = new ArrayList<Portal>();
+    private int iloscRuchow = 0;
+
+    private void przygotujGre() {
+        Portal pierwszy = new Portal();
+        pierwszy.setNazwa("onet.pl");
+        Portal drugi = new Portal();
+        drugi.setNazwa("wp.pl");
+        Portal trzeci = new Portal();
+        trzeci.setNazwa("interia.pl");
+        listaPortali.add(pierwszy);
+        listaPortali.add(drugi);
+        listaPortali.add(trzeci);
+
+        System.out.println("Twoim celem jest zatopienie trzech portali.");
+        System.out.println("onet.pl, wp.pl, interia.pl");
+        System.out.println("Postaraj się je zatopić w jak najmniejszej ilości ruchów");
+
+        for (Portal rozmieszczanyPortal : listaPortali) {
+            ArrayList<String> nowePolozenie = pomocnik.rozmiescPortal(3);
+            rozmieszczanyPortal.setPolaPolozenia(nowePolozenie);
+        }
+    }
+
+    private void rozpocznijGre() {
+        while (!listaPortali.isEmpty()) {
+            String ruchGracza = pomocnik.pobierzDaneWejsciowe("Podaj pole: ");
+            sprawdzRuchGracza(ruchGracza);
+        }
+        zakonczGre();
+    }
+
+    private void sprawdzRuchGracza(String ruch) {
+        iloscRuchow++;
+        String wynik = "pudło";
+
+        for (Portal portalDoSprawdzenia : listaPortali) {
+            wynik = portalDoSprawdzenia.sprawdz(ruch);
+            if (wynik.equals("trafiony")) {
+                break;
+            }
+            if (wynik.equals("zatopiony")) {
+                listaPortali.remove(portalDoSprawdzenia);
+                break;
+            }
+        }
+        System.out.println(wynik);
+    }
+
+    private void zakonczGre() {
+        System.out.println("Wszystkie portale zostały zatopione! Teraz Twoje informacje nie mają znaczenia.");
+        if (iloscRuchow <= 18) {
+            System.out.println("Wykonałeś jedynie " + iloscRuchow + " ruchów.");
+            System.out.println("Wydostałeś się, zanim Twoje informacje zatonely");
+        } else {
+            System.out.println("Ale się grzebałeś. Wykonałeś aż " + iloscRuchow + " ruchów");
+            System.out.println("Teraz rybki pływają pomiędzy Twoimi informacjami");
+        }
+    }
+
+    public static void main (String[] args) {
+        PortalGraMax gra = new PortalGraMax();
+        gra.przygotujGre();
+        gra.rozpocznijGre();
+    }
+
 }
 
 
